bc765c65e5f14195bc5c7762310829e602f12c83
abhayrajpatel <patelabhayraj22@gmail.com>
2025-02-24 22:11:09 -0800

    initial commit

---
+++ js/list.py
@@ -0,0 +1,23 @@
+#t=(40,50,22,60,11,22,44)
+
+#mix =t[0]
+
+#for i in t:
+  #  if i<mix:
+ #       mix  = i
+#print(mix)
+
+
+
+#t=(40,50,22,60,11,22,44)
+
+#min =t[0]
+#
+#for i in t:
+ #   if i>min:
+  #      min = i
+#print(min)
+
+d={'name':'ajay'.'city':'Dewas'}
+for i in d:
+    print(i)
